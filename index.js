263334dfd76eeedb97c7391fff2f1f672db03233
Kavya Saxena <kavyasaxena2811@gmail.com>
2026-07-31 16:10:23 +0530

    Fix PORT issue

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ app.get("/search/:key", async (req, resp) => {
     resp.send(result);
 })
 
-const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
